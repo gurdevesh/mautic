ed37db2c6016785c2476e99a68048748c4e80283
Devesh <Gur.devesh@gmail.com>
2020-06-11 01:46:01 +0530

    going back

--- wp-content/plugins/events-and-news/archive-events.php
+++ wp-content/plugins/events-and-news/archive-events.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying list of News posts 
+ * The template for displaying list of News posts
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
@@ -11,68 +11,76 @@
 
 get_header();
 ?>
-	<section id="primary" class="content-area">
-		<div class="container">
-			<div id="main" class="site-main">
-	            <h2>Events</h2>
-				<?php
-					
-				/* Start the Loop */
-				while ( have_posts() ) :
-					the_post(); ?>
+    <section id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <h2>Events</h2>
+            <?php
 
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<div class="article-left"> 
-							<?php
-							$event_date = get_field('date' );
-		                    if(!empty($event_date)){ ?>
-		                        <br />
-		                        <!-- <strong>Event Date:</strong> -->
-								<?php
-								$current_date = date('l, F jS Y g:i A T');
-								$new_date_format= date( 'j F Y', strtotime($event_date));
-								if ( strtotime($current_date) < strtotime($event_date) ) { ?>
-								<h3>Upcoming On</h3>
-								<?php } ?>
-								<br/>
-								<strong> <?php 
-											//echo esc_html(  $new_date_format );
-											echo $event_date;
-										?> 
-								</strong>
-		                    <?php } ?>
-						</div>
+            /* Start the Loop */
+            while ( have_posts() ) :
+            the_post(); ?>
 
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <h1 class="entry-title">
+                        <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+                    </h1>
+                    <?php
+                    // Edit post link.
+                    edit_post_link(
+                        sprintf(
+                            wp_kses(
+                            /* translators: %s: Name of current post. Only visible to screen readers. */
+                                __( 'Edit <span class="screen-reader-text">%s</span>', 'twentynineteen' ),
+                                array(
+                                    'span' => array(
+                                        'class' => array(),
+                                    ),
+                                )
+                            ),
+                            get_the_title()
+                        ),
+                        '<span class="edit-link">' . twentynineteen_get_icon_svg( 'edit', 16 ),
+                        '</span>'
+                    );
+                    ?>
+                </header>
+                <div class="entry-content">
+                    <?php
+                    $bg = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' );
+                    ?>
+                    <img src="<?php echo $bg[0]; ?>" class="img-responsive" />
+                    <?php  the_excerpt(); ?>
+                    <!--  <a href="--><?php //the_permalink() ?><!--">Read More</a><br />-->
 
-						<header class="entry-header">
-		                    <h1 class="entry-title">
-		                        <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
-		                    </h1>
-							
-						</header>
-						<div class="entry-content">
-		                    <?php
-		                    $bg = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' );
-		                    ?>
-		                    <img src="<?php echo $bg[0]; ?>" class="img-responsive" />
-		                    <?php  the_excerpt(); ?>
-							<!--  <a href="--><?php //the_permalink() ?><!--">Read More</a><br />-->
+                    <?php
+                    $location = get_field( 'location' );
+                    if(!empty($location)){ ?>
+                        <strong>Location:</strong>
+                        <?php  echo get_field( 'location' );
+                    } ?>
 
-							<?php
-		                    $location = get_field( 'location' );
-		                    if(!empty($location)){ ?>
-		                        <strong>Location:</strong>
-		                        <?php  echo get_field( 'location' );
-		                    } ?>
+                    <?php
+                    $event_date = get_field('date' );
+                    if(!empty($event_date)){ ?>
+                        <br />
+                        <!-- <strong>Event Date:</strong> -->
+                        <?php
+//                        $current_date = date('l F jS Y - g:i A');
+                        $current_date = date('l, F jS Y g:i A T');
+                        $new_date_format= date( 'j F Y', strtotime($event_date));
+                        if ( strtotime($current_date) < strtotime($event_date) ) { ?>
+                            <h3>Upcoming On</h3>
+                        <?php } ?>
+                        <br/>
+                        <strong> <?php echo esc_html(  $new_date_format );?> </strong>
+                    <?php } ?>
+                </div><!-- .entry-content -->
+                <?php  endwhile; // End of the loop. ?>
+                <?php wpbeginner_numeric_posts_nav(); ?>
+                <?php filter_archive_year_month('events'); ?>
 
-		                    
-						</div><!-- .entry-content -->
-					</article>
-					<?php  endwhile; // End of the loop. ?>
-	                <?php wpbeginner_numeric_posts_nav(); ?>
-			</div><!-- #main -->
-
-		</div>
-	</section><!-- #primary -->
+        </main><!-- #main -->
+    </section><!-- #primary -->
 <?php
 get_footer();
