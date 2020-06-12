a22773aacdeebf9cc9486babc4cfd4e0800c53d7
Devesh Gur <gur.devesh@gmail.com>
2020-06-13 01:06:29 +0530

    Lost changes

--- wp-content/plugins/events-and-news/event-and-news.php
+++ wp-content/plugins/events-and-news/event-and-news.php
@@ -173,74 +173,6 @@ function si_include_template_function($template_path)
     return $template_path;
 }
 
-function wpbeginner_numeric_posts_nav()
-{
-    if (is_singular())
-        return;
-
-    global $wp_query;
-
-    /** Stop execution if there's only 1 page */
-    if ($wp_query->max_num_pages <= 1)
-        return;
-    $paged = get_query_var('paged') ? absint(get_query_var('paged')) : 1;
-    $max = intval($wp_query->max_num_pages);
-
-    /** Add current page to the array */
-    if ($paged >= 1)
-        $links[] = $paged;
-
-    /** Add the pages around the current page to the array */
-    if ($paged >= 3) {
-        $links[] = $paged - 1;
-        $links[] = $paged - 2;
-    }
-
-    if (($paged + 2) <= $max) {
-        $links[] = $paged + 2;
-        $links[] = $paged + 1;
-    }
-
-    echo '<div class="navigation"><ul>' . "\n";
-
-    /** Previous Post Link */
-    if (get_previous_posts_link())
-        printf('<li>%s</li>' . "\n", get_previous_posts_link());
-
-    /** Link to first page, plus ellipses if necessary */
-    if (!in_array(1, $links)) {
-        $class = 1 == $paged ? ' class="active"' : '';
-
-        printf('<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url(get_pagenum_link(1)), '1');
-
-        if (!in_array(2, $links))
-            echo '<li>…</li>';
-    }
-
-    /** Link to current page, plus 2 pages in either direction if necessary */
-    sort($links);
-    foreach ((array)$links as $link) {
-        $class = $paged == $link ? ' class="active"' : '';
-        printf('<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url(get_pagenum_link($link)), $link);
-    }
-
-    /** Link to last page, plus ellipses if necessary */
-    if (!in_array($max, $links)) {
-        if (!in_array($max - 1, $links))
-            echo '<li>…</li>' . "\n";
-
-        $class = $paged == $max ? ' class="active"' : '';
-        printf('<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url(get_pagenum_link($max)), $max);
-    }
-
-    /** Next Post Link */
-    if (get_next_posts_link())
-        printf('<li>%s</li>' . "\n", get_next_posts_link());
-
-    echo '</ul></div>' . "\n";
-
-}
-
 
 function new_excerpt_more($excerpt ) {
     global $post;
@@ -260,11 +192,13 @@ add_filter('excerpt_more', 'si_new_excerpt_more');
 
 function filter_archive_year_month($type){
     global $wpdb, $wp_locale;
+
     $sql_where = $wpdb->prepare( "WHERE p.post_type = %s AND p.post_status = 'publish' AND pm.meta_key = 'date'", $type );
 
     $query = "SELECT YEAR(pm.meta_value) AS year, MONTH(pm.meta_value) AS month from $wpdb->posts p left join $wpdb->postmeta pm on p.ID = pm.post_id $sql_where GROUP BY year,month ORDER BY YEAR(pm.meta_value) DESC";
-    
+
     $results = $wpdb->get_results( $query );
+
     $year_array = array();
     foreach ($results as $each){
         $year_array[$each->year][] = $each->month;
@@ -276,8 +210,8 @@ function filter_archive_year_month($type){
         ?>
         <ul>
             <lh><a href="<?php echo $url ?>"><?php echo $year ?></a></lh>
-            <?php 
-                foreach($month as $each) {
+            <?php
+            foreach($month as $each) {
                 $url = get_month_link( $year, $each ).$suffix;
                 ?>
                 <li> <a href="<?php echo $url; ?>"><?php echo $wp_locale->get_month( $each ); ?></a> </li>
@@ -286,17 +220,35 @@ function filter_archive_year_month($type){
     <?php }
 }
 
-//add_action( 'init', 'pmg_rewrite_add_rewrites' );
-//function pmg_rewrite_add_rewrites()
-//{
-////    add_rewrite_rule('news/page/([0-9]{1,})/?$','index.php?pagename=news&paged=$matches[1]', 'top');
-//
-//}
-
 add_action('init', function () {
     add_rewrite_rule('news/?$','index.php?pagename=news', 'top');
+    add_rewrite_rule('events/?$','index.php?pagename=events', 'top');
     flush_rewrite_rules();
 }, 1000);
+
+//Load template from specific page
+add_filter( 'page_template', 'wpa3396_page_template' );
+function wpa3396_page_template( $page_template ){
+    if ( get_page_template_slug() == 'archive-news.php' ) {
+        $page_template = dirname( __FILE__ ) . '/archive-news.php';
+    }
+    if ( get_page_template_slug() == 'archive-events.php' ) {
+        $page_template = dirname( __FILE__ ) . '/archive-events.php';
+    }
+    return $page_template;
+}
+
+/**
+ * Add "Custom" template to page attirbute template section.
+ */
+add_filter( 'theme_page_templates', 'wpse_288589_add_template_to_select', 10, 4 );
+function wpse_288589_add_template_to_select( $post_templates, $wp_theme, $post, $post_type ) {
+    // Add custom template named template-custom.php to select dropdown
+    $post_templates['archive-news.php'] = __('News Archive');
+    $post_templates['archive-events.php'] = __('Events Archive');
+
+    return $post_templates;
+}
 
 function si_get_breadcrumbs()
 {
