a7c15fa04fbcf616071e3bb3606d402b31d352c6
Devesh <Gur.devesh@gmail.com>
2020-06-13 22:56:54 +0530

    header - jquery install

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -22,7 +22,10 @@
 		
 		
 -		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous">
-+		
++		<script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
 
 		<?php wp_head(); ?>
 		<script type="text/javascript">
