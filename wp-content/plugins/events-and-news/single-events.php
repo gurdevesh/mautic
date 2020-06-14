bbcd50c8a3fc44052e705845488b478346f7902a
Devesh <Gur.devesh@gmail.com>
2020-06-14 16:55:17 +0530

    changes - detail page

--- wp-content/plugins/events-and-news/single-events.php
+++ wp-content/plugins/events-and-news/single-events.php
@@ -11,47 +11,55 @@
 
 get_header();
 ?>
-    <div class="container">
-        <div class="single-event-wrap">
-            <div class="date-time-wrap"> 
-                <div class="row"> 
-                    
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main">
-            <?php echo do_shortcode("[breadcrumb]"); ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <div class="container">
             <?php
 
             // Start the Loop.
             while (have_posts()) :
                 the_post(); ?>
-
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header">
-                        <strong> <?php echo esc_html(get_field('date')) .' '.  get_field('duration'); ?> </strong>
-                        <h1 class="entry-title">
-                            <?php the_title() ?>
-                        </h1>
-                    </header>
-                    <div class="entry-content">
+            <div class="single-news-page">
+                <div class="breadcrumb-wrap">
+                    <?php echo do_shortcode("[breadcrumb]"); ?>   
+                </div>
+                <div class="news-date-wrap">
+                    <div class="date">
+                        <i class="far fa-calendar-alt"></i>
+                        <?php echo esc_html(  get_field('date' ) ); ?>
+                    </div>
+                    <div class="time">
+                        <i class="far fa-clock"></i>
+                        <?php echo esc_html( human_time_diff( get_the_time('U'), current_time('timestamp') ) ) . ' ago'; ?>
+                    </div>
+                    <div class="category">
+                        <i class="fas fa-sitemap"></i>
                         <?php
-                        $bg = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' );
+                            $terms = get_the_terms( get_the_ID(), 'news_type' );
+                            if(!empty($terms[0])){
+                                $term = $terms[0]; ?>
+                                <a href="<?php echo get_category_link($term->term_id) ?>" ><?php echo $term->name; ?></a>
+                            <?php }
+                            $bg = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' ); 
                         ?>
-                        <img src="<?php echo $bg[0]; ?>" class="img-responsive" />
-                        <?php the_content(); ?>
-                        <br/>
-                        <br/>
-                    </div><!-- .entry-content -->
-                </article><!-- #post-<?php the_ID(); ?> -->
+                    </div>
+                </div>
+                <div class="single-news-title">
+                    <h2> 
+                        <?php the_title() ?>
+                    </h2>
+                </div>
+                <div class="single-news-desc">
+                    <img src="<?php echo $bg[0]; ?>" class="img-responsive" />
+                    <?php the_content(); ?>
+                </div>
+            </div>
             <?php
             endwhile; ?>
-        </main><!-- #main -->
-    </div><!-- #primary -->
+        
+        </div>  
+    </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_footer();
