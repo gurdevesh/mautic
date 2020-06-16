c28e1797d26f7954d877888ea42565a5a2d357b6
Devesh <Gur.devesh@gmail.com>
2020-06-17 01:51:31 +0530

    cookie for active tab

--- wp-content/plugins/events-and-news-oop/public/js/custom-public.js
+++ wp-content/plugins/events-and-news-oop/public/js/custom-public.js
@@ -2,6 +2,7 @@ $(document).on('click', '#myTab li a', function(e){
     e.preventDefault();
     var t = $(this).attr('href');
     var bools = $(this).hasClass('active');
+    setCookie('active-tab',t, 1);
     if(bools === false)
     { //this is the start of our condition 
         $('#myTab li a').removeClass('active');           
@@ -11,3 +12,40 @@ $(document).on('click', '#myTab li a', function(e){
         $(t).fadeIn('slow');
     }
 });
+$(document).ready(function(){
+    var getActiveTab = getCookie('active-tab');
+    if(getActiveTab != '' getActiveTab != undefined){
+        $('#myTab li a').removeClass('active');
+        $('#myTab li a[href="' + getActiveTab + '"]');
+        $(this).addClass('active');
+        $('.tab-pane').hide();
+        $(getActiveTab).fadeIn('slow');
+    }
+    
+})
+
+
+
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
