70eed32eb521dde27b4bd37fc4355267f346d553
Devesh <Gur.devesh@gmail.com>
2020-06-19 00:28:12 +0530

    changes in read more <i class="fas fa-chevron-right"></i>

--- wp-content/plugins/events-and-news-oop/includes/class-event-news-public.php
+++ wp-content/plugins/events-and-news-oop/includes/class-event-news-public.php
@@ -226,7 +226,7 @@ class Event_News_Public {
     function excerpt_with_read_more($excerpt) {
         global $post;
         if(!empty($post->post_excerpt)){
-            return '<p>'.$post->post_excerpt.' <a href="'. get_permalink($post->ID) . '">Read More</a></p>';
+            return '<p>'.$post->post_excerpt.' <a href="'. get_permalink($post->ID) . '">Read More <i class="fas fa-chevron-right"></i></a></p>';
         }
         return $excerpt;
     }
@@ -234,7 +234,7 @@ class Event_News_Public {
 
     function modify_read_more($more) {
         global $post;
-        return ' <a href="'. get_permalink($post->ID) . '">' . 'Read More' . '</a>';
+        return ' <a href="'. get_permalink($post->ID) . '">' . 'Read More <i class="fas fa-chevron-right"></i>' . '</a>';
     }
 
     function show_breadcrumbs()
