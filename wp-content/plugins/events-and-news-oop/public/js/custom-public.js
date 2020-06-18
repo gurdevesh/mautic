db379b3b96fbe9f5d1b8c9b14f4c98b1ff80d65a
Devesh Gur <gur.devesh@gmail.com>
2020-06-18 20:56:46 +0530

    Calendar view - WIP

--- wp-content/plugins/events-and-news-oop/public/js/custom-public.js
+++ wp-content/plugins/events-and-news-oop/public/js/custom-public.js
@@ -65,5 +65,51 @@ $(document).on('click', '.d-year', function(){
 $(document).on('click', '.current-year-month a', function(e){
     e.preventDefault();
     $('.custom-calender').toggleClass('dd-active');
-})
+    var current_year = $('.current-year-month .year-month').attr('data-year');
+    console.log(current_year);
+    var current_month = $('.current-year-month .year-month').attr('data-month');
+    console.log(current_month);
+    if(current_year != 'Year' && current_month != 'Month'){
+        $('.year-wrap .current-year').html(current_year);
+        // $('.months-wrap').removeClass('hidden').addClass('hidden');
+        $('.months-wrap[data-year="'+current_year+'"]').addClass('current').removeClass('hidden');
+    }
+});
 
+$(document).on('click', '.prev-year .fa-chevron-up', function () {
+    var current_month = $('.months-wrap.current');
+    var prev_month = $('.months-wrap.current').prev('.months-wrap');
+    if(prev_month.length > 0){
+        current_month.addClass('hidden').removeClass('current');
+        prev_month.removeClass('hidden').addClass('current');
+        var prev_year = prev_month.attr('data-year');
+        $('.year-wrap .current-year').html(prev_year);
+
+        if(prev_month.hasClass('first')){
+            $('.prev-year').attr('disabled', true);
+            $('.next-year').attr('disabled', false);
+        }
+        else{
+            $('.prev-year').attr('disabled', false);
+        }
+    }
+});
+
+$(document).on('click', '.next-year .fa-chevron-down', function () {
+    var current_month = $('.months-wrap.current');
+    var next_month = $('.months-wrap.current').next('.months-wrap');
+    if(next_month.length > 0){
+        current_month.addClass('hidden').removeClass('current');
+        next_month.removeClass('hidden').addClass('current');
+        var next_year = next_month.attr('data-year');
+        $('.year-wrap .current-year').html(next_year);
+
+        if(next_month.hasClass('last')){
+            $('.next-year').attr('disabled', true);
+            $('.prev-year').attr('disabled', false);
+        }
+        else{
+            $('.next-year').attr('disabled', false);
+        }
+    }
+});
